1ad8511c90cf0a239c6ec75305fc4dd15f99f2fc
Piyushkumar Rajput <rajput9piyush@users.noreply.github.com>
2021-02-10 20:43:37 +0530

    minor changes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
-var app = express();app.get('/', function (req, res) {
+var app = express();
+app.get('/', function (req, res) {
   res.send('<h1>Team E</h1>');
 });app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
